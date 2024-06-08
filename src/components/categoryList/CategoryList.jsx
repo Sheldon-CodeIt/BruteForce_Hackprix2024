53ddea76a815a2b0be42ea82a56f2e3289edc11c
Sheldon <sheldonchettiar3@gmail.com>
2024-06-08 15:40:48 +0530

    setup file structure

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./categoryList.module.css";
+
+const Category = () => {
+  return (
+    <div className={styles.container}>Category</div>
+  )
+}
+
+export default Category
